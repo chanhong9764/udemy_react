cddb05c53656376087862e79a3a04f984bf0b0a3
chanhong9764 <chanhong9784@naver.com>
2022-10-30 22:55:13 +0900

    Udemy React - 가계부 프로젝트

---
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [year, setyear] = useState("2020");
+  const onChangeYearHandler = (year) => {
+    setyear(year);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === year;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={year} onChangeYear={onChangeYearHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
